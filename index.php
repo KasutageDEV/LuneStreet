ce95e79f2c9c6ab3a0bf1454a2f130046b2bf63c
Kasutage <mvasbien.pro@gmail.com>
2022-09-12 20:30:54 +0200

    save 12/09/22 à 20:30

--- index.php
+++ index.php
@@ -65,19 +65,146 @@
 
 		<div class="fil__content">
 			<div class="fil__actus">
-				<div class="fil__post">
-					<div class="fil__post__profil">
-						<div class="fil__post__profil-left">
-							<img src="https://avatars.dicebear.com/api/adventurer-neutral/LS-0000000001.svg" class="fil__post-avatar">
-							<div class="fil__post-infos">
-								<h4>LS-0000000001</h4>
-								<h5>Le 08/09 à 01:56</h5>
+				<div class="post__content">
+					<div class="post">
+						<div class="post__header">
+							<div class="post__header-left">
+								<img src="https://avatars.dicebear.com/api/adventurer-neutral/LS-0000000001.svg" class="post__header-avatar">
+								<div class="post__header-infos">
+									<h4>LS-0000000001</h4>
+									<h5>Le 08/09 à 01:56</h5>
+								</div>
+								<div class="post__header-badge admin">Fondateur</div>
 							</div>
-							<div class="fil__post-badge admin">ADMIN</div>
+							<div class="post__header-right">
+								<a href="#"><i class="fas fa-exclamation-circle"></i> Signaler</a>
+							</div>
 						</div>
-						<div class="fil__post__profil-right">
-							<a href="#"><i class="fas fa-exclamation-circle"></i> Signaler</a>
+						<div class="post__body">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</div>
+						<div class="post__footer">
 							<a href="#"><i class="far fa-heart"></i> J'aime</a>
+							<a href="#"><i class="fas fa-coins"></i> +1 LS Point</a>
+							<a href="#"><i class="far fa-star"></i> Poste pertinent</a>
+						</div>
+					</div>
+					<div class="post__comment">
+						<h1>Commentaires - 1</h1>
+						<form method="POST" action="">
+							<div class="post__comment__form">
+								<input type="text" name="com" placeholder="Répondre à LS-0000000001">
+								<button type="submit" name="submit_com"><i class="fas fa-paper-plane"></i></button>
+							</div>
+						</form>
+						<div class="post__comment__rep">
+							<div class="post__comment__rep-profil">
+								<img src="https://avatars.dicebear.com/api/adventurer-neutral/LS-0000000001.svg" class="post__comment__rep-avatar">
+								<div class="post__comment__rep-infos">
+									<h4>LS-0000000001</h4>
+									<h5>Le 08/09 à 01:56</h5>
+								</div>
+								<div class="post__header-badge admin">Fondateur</div>
+							</div>
+							<div class="post__comment__rep-message">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+							</div>
+						</div>
+
+						<div class="post__comment__rep">
+							<div class="post__comment__rep-profil">
+								<img src="https://avatars.dicebear.com/api/adventurer-neutral/LS-0000000001.svg" class="post__comment__rep-avatar">
+								<div class="post__comment__rep-infos">
+									<h4>LS-0000000001</h4>
+									<h5>Le 08/09 à 01:56</h5>
+								</div>
+							</div>
+							<div class="post__comment__rep-message">
+								LoL.
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="post__content">
+					<div class="post">
+						<div class="post__header">
+							<div class="post__header-left">
+								<img src="https://avatars.dicebear.com/api/adventurer-neutral/LS-0000000001.svg" class="post__header-avatar">
+								<div class="post__header-infos">
+									<h4>LS-0000000001</h4>
+									<h5>Le 08/09 à 01:56</h5>
+								</div>
+								<div class="post__header-badge admin">Fondateur</div>
+							</div>
+							<div class="post__header-right">
+								<a href="#"><i class="fas fa-exclamation-circle"></i> Signaler</a>
+							</div>
+						</div>
+						<div class="post__body">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</div>
+						<div class="post__footer">
+							<a href="#"><i class="far fa-heart"></i> J'aime</a>
+							<a href="#"><i class="fas fa-coins"></i> +1 LS Point</a>
+							<a href="#"><i class="far fa-star"></i> Poste pertinent</a>
+						</div>
+					</div>
+					<div class="post__comment">
+						<h1>Commentaires - 1</h1>
+						<form method="POST" action="">
+							<div class="post__comment__form">
+								<input type="text" name="com" placeholder="Répondre à LS-0000000001">
+								<button type="submit" name="submit_com"><i class="fas fa-paper-plane"></i></button>
+							</div>
+						</form>
+						<div class="post__comment__rep">
+							<div class="post__comment__rep-profil">
+								<img src="https://avatars.dicebear.com/api/adventurer-neutral/LS-0000000001.svg" class="post__comment__rep-avatar">
+								<div class="post__comment__rep-infos">
+									<h4>LS-0000000001</h4>
+									<h5>Le 08/09 à 01:56</h5>
+								</div>
+								<div class="post__header-badge admin">Fondateur</div>
+							</div>
+							<div class="post__comment__rep-message">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+							</div>
+						</div>
+
+						<div class="post__comment__rep">
+							<div class="post__comment__rep-profil">
+								<img src="https://avatars.dicebear.com/api/adventurer-neutral/LS-0000000001.svg" class="post__comment__rep-avatar">
+								<div class="post__comment__rep-infos">
+									<h4>LS-0000000001</h4>
+									<h5>Le 08/09 à 01:56</h5>
+								</div>
+							</div>
+							<div class="post__comment__rep-message">
+								LoL.
+							</div>
 						</div>
 					</div>
 				</div>
@@ -147,7 +274,7 @@
 			<h3>Votre message</h3>
 			<form method="POST" action="">
 				<div class="chat__group">
-					<textarea name="message" placeholder="Message...."></textarea>
+					<textarea name="message" placeholder="Message..."></textarea>
 					<button type="submit" name="submit_message"><i class="fas fa-paper-plane"></i></button>
 				</div>
 			</form>
