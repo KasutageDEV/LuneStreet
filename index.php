ffec43f50452a7933a35b29c0e1eaac64615b938
Kasutage <mvasbien.pro@gmail.com>
2022-09-18 06:14:30 +0200

    Continue

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>LunéStreet - School networks</title>
+	<title>LunéStreet | School networks</title>
 	<meta name="description" content="Réseaux sociale dédié aux établissements scolaire de Lunéville.">
 	<meta name="author" content="KasutageDEV">
 	<meta property="og:locale" content="fr_FR">
@@ -15,272 +15,46 @@
 	<meta property="og:title" content="LunéStreet - School networks">
 	<meta property="og:description" content="Réseaux sociale dédié aux établissements scolaire de Lunéville.">
 	<link rel="canonical" href="#link#">
-	<link rel="stylesheet" type="text/css" href="./@fortawesome/fontawesome-free/css/all.min.css">
-	<link rel="stylesheet" type="text/css" href="./assets/css/global.css">
+	<!-- <link rel="stylesheet" type="text/css" href="../@fortawesome/fontawesome-free/css/all.min.css"> -->
+	<link rel='stylesheet' type="text/css" href='https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css'>
+	<link rel='stylesheet' type="text/css"href='https://cdn-uicons.flaticon.com/uicons-solid-rounded/css/uicons-solid-rounded.css'>
+	<link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-brands/css/uicons-brands.css'>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/brands.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/fontawesome.min.css">
+	<link rel="stylesheet" type="text/css" href="./assets/css/index.css">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
 <body>
 
-<!-- START SIDEBAR -->
-	<div class="sidebar" id="sidebar">
-		<div class="sidebar__avatar">
-			<img src="https://avatars.dicebear.com/api/adventurer-neutral/LS-0000000001.svg" class="sidebar__img">
-		</div>
-		<div class="sidebar__elmt">
-			<h1>LS</h1>
-			<p>0000000001</p>
-		</div>
-		<div class="sidebar__elmt">
-			<h1>Établissement</h1>
-			<p>Saint-pierre fourier</p>
-		</div>
-		<div class="sidebar__elmt">
-			<h1>Classe</h1>
-			<p>3<sup>ème</sup></p>
-		</div>
-		<div class="sidebar__elmt">
-			<h1>LS Points</h1>
-			<p>1000</p>
-		</div>
-		<a href="#" class="sidebar__logout">Déconnexion</a>
-	</div>
-<!-- END SIDEBAR -->
-
-
-
-
-
-<!-- START FIL -->
-	<div class="fil" id="fil">
-		<header class="fil__header">
-			<a href="#"><img src="./assets/imgs/Logo_t.png" class="fil__header-logo"></a>
-			<div class="fil__header-nav">
-				<a href="#" class="fil__header-link"><i class="fas fa-home"></i>Accueil</a>
-				<a href="#" class="fil__header-link"><i class="fas fa-building"></i>Mon établissement</a>
-			</div>
-			<button type="button" class="fil__header-button"><i class="fas fa-plus"></i> Nouveau post</button>
-		</header>
-
-		<div class="fil__content">
-			<div class="fil__actus">
-				<div class="post__content">
-					<div class="post">
-						<div class="post__header">
-							<div class="post__header-left">
-								<img src="https://avatars.dicebear.com/api/adventurer-neutral/LS-0000000001.svg" class="post__header-avatar">
-								<div class="post__header-infos">
-									<h4>LS-0000000001</h4>
-									<h5>Le 08/09 à 01:56</h5>
-								</div>
-								<div class="post__header-badge admin">Fondateur</div>
-							</div>
-							<div class="post__header-right">
-								<a href="#"><i class="fas fa-exclamation-circle"></i> Signaler</a>
-							</div>
-						</div>
-						<div class="post__body">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-						</div>
-						<div class="post__footer">
-							<a href="#"><i class="far fa-heart"></i> J'aime</a>
-							<a href="#"><i class="fas fa-coins"></i> +1 LS Point</a>
-							<a href="#"><i class="far fa-star"></i> Poste pertinent</a>
-						</div>
-					</div>
-					<div class="post__comment">
-						<h1>Commentaires - 1</h1>
-						<form method="POST" action="">
-							<div class="post__comment__form">
-								<input type="text" name="com" placeholder="Répondre à LS-0000000001">
-								<button type="submit" name="submit_com"><i class="fas fa-paper-plane"></i></button>
-							</div>
-						</form>
-						<div class="post__comment__rep">
-							<div class="post__comment__rep-profil">
-								<img src="https://avatars.dicebear.com/api/adventurer-neutral/LS-0000000001.svg" class="post__comment__rep-avatar">
-								<div class="post__comment__rep-infos">
-									<h4>LS-0000000001</h4>
-									<h5>Le 08/09 à 01:56</h5>
-								</div>
-								<div class="post__header-badge admin">Fondateur</div>
-							</div>
-							<div class="post__comment__rep-message">
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-							</div>
-						</div>
-
-						<div class="post__comment__rep">
-							<div class="post__comment__rep-profil">
-								<img src="https://avatars.dicebear.com/api/adventurer-neutral/LS-0000000001.svg" class="post__comment__rep-avatar">
-								<div class="post__comment__rep-infos">
-									<h4>LS-0000000001</h4>
-									<h5>Le 08/09 à 01:56</h5>
-								</div>
-							</div>
-							<div class="post__comment__rep-message">
-								LoL.
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="post__content">
-					<div class="post">
-						<div class="post__header">
-							<div class="post__header-left">
-								<img src="https://avatars.dicebear.com/api/adventurer-neutral/LS-0000000001.svg" class="post__header-avatar">
-								<div class="post__header-infos">
-									<h4>LS-0000000001</h4>
-									<h5>Le 08/09 à 01:56</h5>
-								</div>
-								<div class="post__header-badge admin">Fondateur</div>
-							</div>
-							<div class="post__header-right">
-								<a href="#"><i class="fas fa-exclamation-circle"></i> Signaler</a>
-							</div>
-						</div>
-						<div class="post__body">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-						</div>
-						<div class="post__footer">
-							<a href="#"><i class="far fa-heart"></i> J'aime</a>
-							<a href="#"><i class="fas fa-coins"></i> +1 LS Point</a>
-							<a href="#"><i class="far fa-star"></i> Poste pertinent</a>
-						</div>
-					</div>
-					<div class="post__comment">
-						<h1>Commentaires - 1</h1>
-						<form method="POST" action="">
-							<div class="post__comment__form">
-								<input type="text" name="com" placeholder="Répondre à LS-0000000001">
-								<button type="submit" name="submit_com"><i class="fas fa-paper-plane"></i></button>
-							</div>
-						</form>
-						<div class="post__comment__rep">
-							<div class="post__comment__rep-profil">
-								<img src="https://avatars.dicebear.com/api/adventurer-neutral/LS-0000000001.svg" class="post__comment__rep-avatar">
-								<div class="post__comment__rep-infos">
-									<h4>LS-0000000001</h4>
-									<h5>Le 08/09 à 01:56</h5>
-								</div>
-								<div class="post__header-badge admin">Fondateur</div>
-							</div>
-							<div class="post__comment__rep-message">
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-							</div>
-						</div>
-
-						<div class="post__comment__rep">
-							<div class="post__comment__rep-profil">
-								<img src="https://avatars.dicebear.com/api/adventurer-neutral/LS-0000000001.svg" class="post__comment__rep-avatar">
-								<div class="post__comment__rep-infos">
-									<h4>LS-0000000001</h4>
-									<h5>Le 08/09 à 01:56</h5>
-								</div>
-							</div>
-							<div class="post__comment__rep-message">
-								LoL.
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="fil__top">
-				<h1 class="fil__top-title">TOP 10 LS POINTS</h1>
-				<div class="fil__top-item">
-					<div class="fil__top__user">
-						<img src="https://avatars.dicebear.com/api/adventurer-neutral/LS-0000000001.svg" class="fil__top__user-avatar">
-						<div class="fil__top__user-infos">
-							<h4>LS-0000000001</h4>
-							<h5><b>1000</b> LS Points</h5>
-						</div>
-					</div>
-					<div class="fil__top__num premier">
-						1
-					</div>
-				</div>
-				<div class="fil__top-item">
-					<div class="fil__top__user">
-						<img src="https://avatars.dicebear.com/api/adventurer-neutral/LS-0000000001.svg" class="fil__top__user-avatar">
-						<div class="fil__top__user-infos">
-							<h4>LS-0000000001</h4>
-							<h5><b>1000</b> LS Points</h5>
-						</div>
-					</div>
-					<div class="fil__top__num premier">
-						1
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-<!-- END FIL -->
-
-
-
-
-
-<!-- START CHAT -->
-	<div class="chat" id="chat">
-		<div class="chat__header">
-			Chat en direct
-		</div>
-
-		<div class="chat__content">
-			<div class="chat__message">
-				<form method="POST" action="">
-					<input type="hidden" name="message_id" value="1">
-					<button class="chat__delete"><i class="fas fa-times"></i></button>
-				</form>
-				<div class="chat__message-profil">
-					<img src="https://avatars.dicebear.com/api/adventurer-neutral/LS-0000000001.svg" class="chat__message-avatar">
-					<div class="chat__message-infos">
-						<h4>LS-0000000001</h4>
-						<h5>Le 08/09 à 01:56</h5>
-					</div>
-					<div class="chat__message-badge admin">ADMIN</div>
-				</div>
-				<div class="chat__message-text">
-					Lorem ipsum deydgeyfd uyegdeuzbdfuehf efdjshdf hfrh hfrhgrfgfhbfvhsdrfv rhfbrs. Lorem ipsum deydgeyfd uyegdeuzbdfuehf efdjshdf hfrh hfrhgrfgfhbfvhsdrfv rhfbrs
-				</div>
-			</div>
-		</div>
-
-		<div class="chat__form">
-			<h3>Votre message</h3>
+	<div class="index__content">
+		<div class="index__left">
+			<h1 class="index__left-title">Je me connecte</h1>
+			<h3 class="index__slogan">Jeter de l’ombre 👻 comme des confettis 🎉</h3>
 			<form method="POST" action="">
-				<div class="chat__group">
-					<textarea name="message" placeholder="Message..."></textarea>
-					<button type="submit" name="submit_message"><i class="fas fa-paper-plane"></i></button>
+				<div class="index__left-group">
+					<label>E-mail</label>
+					<input type="text" name="email">
+				</div>
+				<div class="index__left-group">
+					<label>Mot de passe</label>
+					<input type="password" name="password">
+				</div>
+				<div class="index__left-btns">
+					<button type="submit" class="btns btns-blue">Connexion</button>
+					<a href="#" class="btns btns-orange">Je m'inscrit</a>
 				</div>
 			</form>
+			<a href="#" class="index__left-forgot">Mot de passe oublié ?</a>
 		</div>
+		<div class="index__right">
+			<img src="./assets/imgs/Logo.png" class="index__right-logo">
+		</div>
+		<a href="https://www.instagram.com/lunestreet54/" target="_blank" class="index__insta">
+			<i class="fa-brands fa-instagram"></i> lunestreet54
+		</a>
 	</div>
-<!-- END CHAT -->
 
 </body>
 </html>
